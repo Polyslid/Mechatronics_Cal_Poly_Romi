46c81b5a47dff7cffff13c573b169b18aad1d88c
Jaymachine63 <jasonreyes_63@yahoo.com>
2025-03-16 19:52:15 -0700

    Delete variables_e.js

--- variables_e.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['start_5ftime_0',['start_time',['../class_encoder_1_1_encoder.html#a85211c812f3113943a4e65b9f7adef41',1,'Encoder::Encoder']]],
-  ['status_1',['status',['../class_i_m_u_1_1_i_m_u.html#a9178beaf4c9386d0ddda16ccf84d8efd',1,'IMU::IMU']]],
-  ['status_5fdata_2',['status_data',['../class_i_m_u_1_1_i_m_u.html#a30d8a0f807d4cb094c00a5d8f25563df',1,'IMU::IMU']]],
-  ['sys_5fstatus_3',['sys_status',['../class_i_m_u_1_1_i_m_u.html#a329f6ad2a84de70f384c39f69085a294',1,'IMU::IMU']]]
-];
