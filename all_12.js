77a43769a38dd062d21324efe18d29404c4ba6d7
Jaymachine63 <jasonreyes_63@yahoo.com>
2025-03-16 19:37:20 -0700

    Delete all_12.js

--- all_12.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['uart_0',['uart',['../namespacemain.html#a20f34167ce94fc3bd45d24f2af4d827a',1,'main']]],
-  ['update_1',['update',['../class_encoder_1_1_encoder.html#a3edf3fe66f8af2e5667a0903452fa49d',1,'Encoder.Encoder.update()'],['../class_line___sensor_1_1_line___sensor.html#a588890cf297b5da8d566188a8040a18e',1,'Line_Sensor.Line_Sensor.update()']]],
-  ['user_2',['user',['../namespacemain.html#a0e87499755628904e3332a2c264dcef6',1,'main']]]
-];
