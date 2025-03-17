c9bba2d622b321ed7ea309741aa026a0c9bc9466
Jaymachine63 <jasonreyes_63@yahoo.com>
2025-03-16 19:43:07 -0700

    Delete variables_f.js

--- variables_f.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['task1_0',['task1',['../namespacemain.html#af4b8f4290f8d32e70654f6deb864787f',1,'main']]],
-  ['task2_1',['task2',['../namespacemain.html#a99d189b8f9eb3784cf7d4e5c3241b562',1,'main']]],
-  ['task3_2',['task3',['../namespacemain.html#a120516a5d42c212c898d4616d266077a',1,'main']]],
-  ['tim_3',['tim',['../class_encoder_1_1_encoder.html#a382ad8b72c52f9f092e7da87a55a17a5',1,'Encoder::Encoder']]],
-  ['tim4_4',['tim4',['../namespacemain.html#a53ed0320302e49d0f03d3eafd9f88dd8',1,'main']]]
-];
