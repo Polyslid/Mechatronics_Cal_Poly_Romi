22c1d3a7c1e428bccfdc6295c5ba72eb91b5f599
Jaymachine63 <jasonreyes_63@yahoo.com>
2025-03-16 19:50:58 -0700

    Delete functions_8.js

--- functions_8.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['read_5fangular_5fvel_0',['read_angular_vel',['../class_i_m_u_1_1_i_m_u.html#afb4cda0933044a70b4e9a99d565306d4',1,'IMU::IMU']]],
-  ['read_5feuler_1',['read_Euler',['../class_i_m_u_1_1_i_m_u.html#a8b97a2ae03f561f73508f11a4d5273f9',1,'IMU::IMU']]],
-  ['reset_5fpid_2',['reset_PID',['../class_p_i_d_1_1_p_i_d.html#af12c9efea285215d1fa0ce0cd99d817e',1,'PID::PID']]]
-];
